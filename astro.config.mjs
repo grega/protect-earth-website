cfe07d6e9f627d3d6e02feaca47fbda227fd09b4
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2026-06-02 10:33:38 +0100

    fix: redirects failing in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,22 @@ export default defineConfig({
 	output: 'static',
 	adapter: netlify(),
 
+	redirects: {
+		'/articles/deep-roots-ancient-woodlands-matter': '/articles/restoring-ancient-woodlands',
+		'/articles/restoring-ancient-woodlands-a-practical-guide-from-high-wood':
+			'/articles/restoring-ancient-woodlands',
+		'/articles/misconception-planting-in-straight-lines-is-lazy-or-bad':
+			'/articles/misconceptions-about-tree-planting',
+		'/events/high-wood-summer-fair-14th-june-2026':
+			'/events/high-wood-summer-fair-liskeard-cornwall-1986120946215.md',
+		'/bath': '/warleigh-nature-reserve',
+		'/blog/[slug]': '/articles/[slug]',
+		'/projects/[slug]': '/sites/[slug]',
+		'/blog': '/articles',
+		'/donate': '/act-now/general-donation',
+		'/projects': '/sites',
+	},
+
 	vite: {
 		plugins: [yaml()],
 	},
